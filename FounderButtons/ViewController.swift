9e7a763c1dadfd3368aa1f98e57dc99feaaba6bd
Alberto Medina <albertomedina@MacBook-Pro-8.local>
2019-01-23 17:38:53 -0500

    Initial Commit

---
+++ FounderButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FounderButtons
+//
+//  Created by Alberto Medina on 1/23/19.
+//  Copyright © 2019 Alberto Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
